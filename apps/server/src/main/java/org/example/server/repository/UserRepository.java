9f30e6573fb51306037c71db8706f3015a3e97e2
Igo <igovieira87@gmail.com>
2025-09-26 09:36:48 -0300

    feat:add UserController

--- apps/server/src/main/java/org/example/server/repository/UserRepository.java
+++ apps/server/src/main/java/org/example/server/repository/UserRepository.java
@@ -2,6 +2,8 @@ package org.example.server.repository;
 
 import org.example.server.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface UserRepository extends JpaRepository<User, Long> {
 }
