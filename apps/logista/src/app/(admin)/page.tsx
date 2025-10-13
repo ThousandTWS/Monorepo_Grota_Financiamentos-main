20defcd6ffd776d62f493f3f6bd00b4165b9d38d
Almaz Digital <vinicius@sampaioforce.com.br>
2025-10-13 09:21:33 -0300

    ajuste no layout.tsx

--- apps/logista/src/app/(admin)/page.tsx
+++ apps/logista/src/app/(admin)/page.tsx
@@ -5,8 +5,8 @@ import React from "react";
 
 export const metadata: Metadata = {
   title:
-    "Next.js E-commerce Dashboard | TailAdmin - Next.js Dashboard Template",
-  description: "This is Next.js Home for TailAdmin Dashboard Template",
+    "Area do logista - Grota Financiamentos",
+  description: "Dashboard do logista - Grota Financiamentos",
 };
 
 export default function Dashboard() {
