6f13521c5710d5be568fd912673f0edaaf35977c
Almaz Digital <vinicius@sampaioforce.com.br>
2025-10-13 10:07:43 -0300

    fix

--- apps/admin/app/(admin)/visao-geral/page.tsx
+++ apps/admin/app/(admin)/visao-geral/page.tsx
@@ -1,5 +1,27 @@
-export default function Page() {
-    return (
-        <div>Visão Geral</div>
-    )
+import React from "react";
+
+export default function Dashboard() {
+  return (
+    <div className="grid grid-cols-12 gap-4 md:gap-6">
+      <div className="col-span-12 space-y-6 xl:col-span-7">
+        
+      </div>
+
+      <div className="col-span-12 xl:col-span-5">
+        
+      </div>
+
+      <div className="col-span-12">
+        
+      </div>
+
+      <div className="col-span-12 xl:col-span-5">
+        
+      </div>
+
+      <div className="col-span-12 xl:col-span-7">
+        
+      </div>
+    </div>
+  );
 }
---
+++ apps/logista/app/(logista)/gestao-clientes/page.tsx
@@ -0,0 +1,7 @@
+export default function GestaoClientes() {
+    return (
+        <div>
+            Gestão de Clientes
+        </div>
+    )
+}
