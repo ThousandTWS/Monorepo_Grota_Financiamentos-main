717c9541ec721a60b02141558236ed253e6e2a29
MarcioJorgeMelo <marciojorgemedeirosmelofilho@gmail.com>
2025-10-23 20:01:33 -0300

    chore: reajust path to localhost tests

--- apps/admin/src/application/services/server/api.ts
+++ apps/admin/src/application/services/server/api.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from "axios";
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+const BASE_URL = "http://localhost:8080/api/v1/grota-financiamentos";
 
 const api: AxiosInstance = axios.create({
   baseURL: BASE_URL,
--- apps/client/src/application/services/server/api.ts
+++ apps/client/src/application/services/server/api.ts
@@ -1,14 +1,13 @@
 import axios, { AxiosInstance } from "axios";
 
-const BASE_URL =  process.env.NEXT_PUBLIC_API_URL;
+const BASE_URL = "http://localhost:8080/api/v1/grota-financiamentos";
 
 const api: AxiosInstance = axios.create({
-    baseURL: BASE_URL,
-    headers: {
-        "Content-Type": "application/json",
-    },
-    withCredentials: true,
+  baseURL: BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
 });
-
 
 export default api;
