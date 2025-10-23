fe8c1631f20f3edb78fdf850e2d1a1e923e5812b
MarcioJorgeMelo <marciojorgemedeirosmelofilho@gmail.com>
2025-10-23 19:53:26 -0300

    feat: add credentials on header´s route API

--- apps/logista/src/application/services/UserServices/UserServices.ts
+++ apps/logista/src/application/services/UserServices/UserServices.ts
@@ -2,7 +2,10 @@ import api from "../server/api";
 
 const userServices = {
   async me() {
-    const { data } = await api.get("/auth/me");
+    const { data } = await api.get("/auth/me", {
+      withCredentials: true
+    });
+
     return data;
   },
 };
