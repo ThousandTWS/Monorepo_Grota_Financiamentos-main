142dc797d23d38e2dc7166af90d5899a09e411dc
MarcioJorgeMelo <marciojorgemedeirosmelofilho@gmail.com>
2025-10-23 19:50:13 -0300

    feat: create auth middleware on logista application

---
+++ apps/logista/middleware.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export async function middleware(request: NextRequest) {
+  const token = request.cookies.get('access_token')?.value;
+  if (!token) return NextResponse.redirect(new URL('http://localhost:3000', request.url));
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/((?!_next|favicon.ico).*)'],
+};
