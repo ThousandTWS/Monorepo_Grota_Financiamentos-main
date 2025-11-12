c0ced13b1d0643da12b9a4095d7bb18efe801fff
Igo <igovieira87@gmail.com>
2025-11-11 21:22:53 -0300

    config do banco

--- apps/server/src/main/java/org/example/server/config/CorsConfig.java
+++ apps/server/src/main/java/org/example/server/config/CorsConfig.java
@@ -9,7 +9,12 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:3001", "http://localhost:3000", "http://localhost:3002")
+                .allowedOriginPatterns(
+                        "http://localhost:3001",
+                        "http://localhost:3000",
+                        "http://localhost:3002",
+                        "https://servidor-grotafinanciamentos.up.railway.app"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE","PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
