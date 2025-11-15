fbf1ece0c7078975da02b7b2e9efb9384035ef7b
MarcioJorgeMelo <marciojorgemedeirosmelofilho@gmail.com>
2025-11-15 18:05:36 -0300

    feat: add integration reset-password-page

--- apps/admin/src/application/core/@types/auth/Props/ResetPasswordPageProps.ts
+++ apps/admin/src/application/core/@types/auth/Props/ResetPasswordPageProps.ts
@@ -2,6 +2,4 @@ export interface ResetPasswordPageProps {
   title?: React.ReactNode
   description?: React.ReactNode
   heroImageSrc?: string
-  onResetPassword?: (event: React.FormEvent<HTMLFormElement>) => void
-  onBackToSignIn?: () => void
 }
