aecdacff24edf6eb543f1534cb00a8f04beaa99b
ThousandTWS <thousandtws@gmail.com>
2025-12-01 18:44:33 -0300

    fix

--- apps/dealer-portal/src/lib/masks.ts
+++ apps/dealer-portal/src/lib/masks.ts
@@ -1,6 +1,6 @@
 export function maskCPF(value: string) {
   return value
-    .replace(/\D/g, "")                      // remove tudo que não é número
+    .replace(/\D/g, "")                     
     .replace(/(\d{3})(\d)/, "$1.$2")        // coloca o primeiro ponto
     .replace(/(\d{3})(\d)/, "$1.$2")        // coloca o segundo ponto
     .replace(/(\d{3})(\d{1,2})$/, "$1-$2")  // coloca o traço
