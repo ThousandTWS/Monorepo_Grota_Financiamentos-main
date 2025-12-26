9653088ecd6e754dca8dff396285b7cfb68cdd1f
Vinícius Oliveira Moreira <vinicius.softengineer@outlook.com>
2025-12-26 00:08:56 -0300

    fix

--- apps/admin-console/src/presentation/features/gestao-veiculos/index.ts
+++
@@ -1,4 +0,0 @@
-export { VehiclesTable } from "./components/VehiclesTable";
-export { VehicleFilters } from "./components/VehicleFilters";
-export { VehicleStats } from "./components/VehicleStats";
-export { VehicleDetailsDialog } from "./components/VehicleDetailsDialog";
